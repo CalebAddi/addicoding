7bba69d260d807275f04284c00b10fa942777427
Caleb Addison <caleb.addi.77@gmail.com>
2025-06-21 21:48:19 -0500

    small updates to Navbar.svelte

--- src/lib/components/apps/Navbar.svelte
+++ src/lib/components/apps/Navbar.svelte
@@ -1,23 +1,23 @@
-<main>
-    <header id="header">
-    <div class="content">
-        <div class="inner">
-            <h1>Dive Into My Portfolio</h1>
+<main class="nav-container">
+    <header id="header" class="header-container">
+        <div class="content">
+            <div class="inner">
+                <h1>Dive Into My Portfolio</h1>
+            </div>
         </div>
-    </div>
-    <nav>
-        <ul id="nav-btns" class="nav-buttons">
-            <li class="nav-li longword-li"><a id="nav-link" href="#professional_work" data-text-small="Professional">Professional Work</a></li>
-            <li class="nav-li longword-li"><a id="nav-link" href="#personal_work" data-text-small="Projects">Personal Projects</a></li>
-            <li class="nav-li"><a id="nav-link" href="#about">About</a></li>
-            <li class="nav-li"><a id="nav-link" href="#contact">Contact</a></li>
-        </ul>
-    </nav>
-</header>
+        <nav>
+            <ul id="nav-btns" class="nav-buttons">
+                <li class="nav-li longword-li"><a id="nav-link" href="#professional_work" data-text-small="Professional">Professional Work</a></li>
+                <li class="nav-li longword-li"><a id="nav-link" href="#personal_work" data-text-small="Projects">Personal Projects</a></li>
+                <li class="nav-li"><a id="nav-link" href="#about">About</a></li>
+                <li class="nav-li"><a id="nav-link" href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
 </main>
 
 <style>
-    main
+    .nav-container
     {
         margin-top: 10.5em;
         padding-bottom: 12em;
@@ -25,7 +25,7 @@
         border: solid 2.5px rgba(215, 200, 200, 0.7);
     }
 
-    header
+    .header-container
     {
         margin-top: 4em;
         padding-top: 2em;
