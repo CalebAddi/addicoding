e57a449dcb419acc8816001ff7821c229777c546
CalebAddi <caleb.addi.77@gmail.com>
2025-06-24 19:43:03 -0500

    forgot to name the resume link

--- src/lib/components/apps/Resume.svelte
+++ src/lib/components/apps/Resume.svelte
@@ -1,7 +1,7 @@
 <main>
     <div class="resume_container">
         <h3>Download Resume</h3>
-        <li class="linkIcons"><a href="assets/download/CalebAddiGameResume.pdf" download class="icon brands" id="resume-icon" style="font-style: 3em;" aria-label="Download Resume PDF">
+        <li class="linkIcons"><a href="assets/download/CalebAddiResume2025.pdf" download class="icon brands" id="resume-icon" style="font-style: 3em;" aria-label="Download Resume PDF">
         <i class="fa-solid fa-arrow-up-from-bracket"></i></a></li>
     </div>
 </main>
